b4701f3a74ea33dee047411085c83a7baa85ea1e
GianmarcoPerini <g.marco.perini@gmail.com>
2021-12-18 01:19:52 +0100

    reloading game button implement

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const newGame = document.querySelector(".btn--new");
 const roll = document.querySelector(".btn--roll");
 const hold = document.querySelector(".btn--hold");
+
 const player1 = document.querySelector(".player--0");
 const player2 = document.querySelector(".player--1");
+
+const score1 = document.querySelector("#score--0");
+const score2 = document.querySelector("#score--1");
+
+const current1 = document.querySelector("#current--0");
+const current2 = document.querySelector("#current--1");
+
 const dice = document.querySelector("#dice");
 
 let active = 0; // player1 = true || player2 = false
@@ -11,7 +19,6 @@ let completeScore = [0, 0];
 let winner = false;
 
 roll.addEventListener("click", () => {
-  if (winner) disableButton();
   // roll & display the roll
   let diceRand = getRandom(1, 6);
   dice.src = `dice-${diceRand}.png`;
@@ -33,43 +40,60 @@ hold.addEventListener("click", () => {
   // if we have a winner
   if (completeScore[active] >= 10) {
     setWinner();
-    disableButton();
+    setButton(true);
+    dice.classList.add("hidden");
   } else {
     // change active player
     changeActive();
   }
 });
 
-newGame.addEventListener("click", () => reload()); // reload page
-
 function getRandom(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-function reload() {
-  location.reload();
-}
+
+newGame.addEventListener("click", reload);
 
 function setWinner() {
   document.querySelector(`#score--${active}`).innerHTML = "🎉";
   document.querySelector(`#score--${active == 0 ? 1 : 0}`).innerHTML = "💥";
-  document
-    .querySelector(`.player--${active}`)
-    .classList.add("player--winner")
-    .add("player--winner name");
+  document.querySelector(`.player--${active}`).classList.add("player--winner");
+
   winner = true;
 }
 
 function changeActive() {
+  partialScore = 0;
   player1.classList.toggle("player--active");
   player2.classList.toggle("player--active");
-  partialScore = 0;
   document.querySelector(`#current--${active}`).innerHTML = 0;
+
   active = active == 0 ? 1 : 0;
 }
 
-function disableButton() {
-  roll.disabled = true;
-  hold.disabled = true;
+function setButton(boolean) {
+  roll.disabled = boolean;
+  hold.disabled = boolean;
+}
+
+function reload() {
+  active = 0;
+  winner = false;
+  partialScore = 0;
+  completeScore = [0, 0];
+
+  score1.textContent = 0;
+  score2.textContent = 0;
+  current1.textContent = 0;
+  current2.textContent = 0;
+
+  player1.classList.add("player--active");
+  player2.classList.remove("player--active");
+  player1.classList.remove("player--winner");
+  player2.classList.remove("player--winner");
+  dice.classList.remove("hidden");
+
+  setButton(false);
 }
